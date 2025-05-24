6073c5a94c75c9c890a7258a935471538a88315a
fkoki0309 <fkoki0309@gmail.com>
2025-05-24 23:49:29 +0900

    完成

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,21 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+
+  let promise = new Promise(function (resolve) {
+    setTimeout(() => resolve(userList), 3000)
+  });
+
+  await promise;
+
+  return userList.map(test);
 }
 
-function test() {
-  return
+function test(data) {
+
+  const full_name = data.family_name + ' ' + data.first_name;
+  data = { ...data, full_name: full_name };
+  return data;
+
 }
 
